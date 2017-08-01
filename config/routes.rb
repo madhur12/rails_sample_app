51636674378cbe902582588f932d7e595d62a67d
Madhur <p.madhur3@yahoo.co.in>
2017-08-01 22:01:26 +0000

    Layout changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
-  get 'foo/bar'
-
-  get 'foo/baz'
-
-  get 'static_pages/home'
-
-  get 'static_pages/help'
   
-  get 'static_pages/about'
+  get 'users/new'
+
+  get '/help', to:'static_pages#help'
   
-  get 'static_pages/contact'
+  get '/about', to:'static_pages#about'
+  
+  get '/contact', to:'static_pages#contact'
+  
+  get '/signup', to:'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,26 +6,20 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base = "Ruby on Rails Tutorial Sample App"
   end
   
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-    assert_select "title", @base
-  end
-
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help | #{@base}"
   end
   
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | #{@base}"
   end
   
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select "title", "Contact | #{@base}"
   end
